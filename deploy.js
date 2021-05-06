d55d2c4e1df78af1acefe56bfd9d83ef9b9278ff
SuperMido <supermido1996@gmail.com>
2021-05-06 16:41:59 +0700

    Init

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'crack material early hotel faculty harsh bless anger arm cattle choose lazy',
+    'https://rinkeby.infura.io/v3/6cf0a4c7810f4dd6adb381921227386d'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode, arguments: ['Hi Kim!']
+        })
+        .send({
+            gas: '1000000',
+            from: accounts[0]
+        });
+
+    console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
