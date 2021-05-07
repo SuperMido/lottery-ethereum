aae846fd3e7ef5e37cce06f352ad14cd32053ab8
SuperMido <supermido1996@gmail.com>
2021-05-07 11:45:17 +0700

    Add Mocha test for deploy contract

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -7,38 +7,25 @@ const web3 = new Web3(provider);
 
 const { interface, bytecode } = require('../compile');
 
+let lottery;
 let accounts;
-let inbox;
 
-beforeEach( async () => {
+beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({
-            data: bytecode, 
-            arguments: ['Hi there!']
-        })
-        .send({
-            from: accounts[0], 
-            gas: '1000000'
-        });
-
-    inbox.setProvider(provider);
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
+                                .deploy({
+                                    data: bytecode
+                                })
+                                .send({
+                                    from: accounts[0],
+                                    gas: '1000000'
+                                });
 });
 
-describe('Inbox', () => {
-    it('deploys a contract', () => {
-        assert.ok(inbox.options.address);      
-    });
 
-    it('has a default message', async () => {
-        const message = await inbox.methods.message().call();
-        assert.equal(message, 'Hi there!');
-    });
-
-    it('can change the message', async () => {
-        await inbox.methods.setMessage('Bye!').send({ from: accounts[0]});
-        const message = await inbox.methods.message().call();
-        assert.equal(message, 'Bye!');
+describe('Lottery Contract', () => {
+    it('deploy a contract', () => {
+        assert.ok(lottery.options.address);
     });
 });
