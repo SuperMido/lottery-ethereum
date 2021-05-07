a449c846baa993d7e466f0f3e9cdabce33eb9107
SuperMido <supermido1996@gmail.com>
2021-05-07 13:34:45 +0700

    Try catch Assertions

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -68,4 +68,17 @@ describe('Lottery Contract', () => {
         assert.equal(accounts[2], players[2]);
         assert.equal(3, players.length);
     });
+
+    it('requires a minimun amount of ether to enter', async () => {
+        try {
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: 0
+            });
+            assert(false);
+        } catch (error) {
+            assert(error);
+        }
+
+    });
 });
